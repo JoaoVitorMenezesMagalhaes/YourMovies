ddbfba0800c168356dc0e89242d33db1270a56d7
rafaelem2 <rafaelem2@al.insper.edu.br>
2021-11-15 23:48:15 -0300

    atualiza

--- yourmovies/src/components/MovieRow.js
+++ yourmovies/src/components/MovieRow.js
@@ -1,9 +1,11 @@
 import React, {useState} from 'react';
 import './MovieRow.css';
+import axios from "axios";
 
 
 export default ({title, items}) => {
     const [barX, setBarX] = useState(0);
+    const [id, setId] = useState("");
 
     const moveLeft = () => {
         let backX = barX + Math.round(window.innerWidth / 2)
@@ -21,14 +23,15 @@ export default ({title, items}) => {
         setBarX(forwardX)
     }
 
+
     return (
         <div className="movieRow">
             <h2> {title} </h2>
             <div className="leftArrow" onClick={moveLeft}>
-                <img className="l" src="images/leftArrow.png"/>
+                <img className="l" src="/images/leftArrow.png"/>
             </div>
             <div className="rightArrow" onClick={moveRight}>
-                <img className="r" src="images/rightArrow.png"/>
+                <img className="r" src="/images/rightArrow.png"/>
             </div>
             <div className="movRowListArea">
                 <div className="movRowList" style={{
@@ -38,7 +41,11 @@ export default ({title, items}) => {
                     {items.results.length > 0 && items.results.map((item, key)=>(
                         <div key={key} className="movRowItem">
                             <img src={`https://image.tmdb.org/t/p/w300${item.poster_path}`} alt={item.original_title}/>
-                            <a href={`/list/add/${item.id}`} className="listBtn">+ Minha Lista</a>
+                            <button className="listBtn" onClick={() => 
+                                axios.post(`https://secure-lake-79874.herokuapp.com/api/favorite/`, {title: item.poster_path}).then((response) => {
+                                    console.log(response.data.title)
+                                })
+                            }> + Minha Lista</button>
                         </div>
                     ))}
                 </div>
